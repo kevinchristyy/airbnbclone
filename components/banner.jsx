f8a3f6ef72f49fd5202786cb563155c0e9f04fe8
kevinchristyy <kevin.10@hotmail.no>
2022-12-28 15:30:04 +0100

    airbnb final

--- components/banner.jsx
+++ components/banner.jsx
@@ -2,15 +2,14 @@ import Image from "next/image";
 
 function Banner() {
   return (
-    <div className="relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px]">
-      <Image
+    <div className="relative h-[300px] sm:h-[400px] lg:h-[300px] xl:h-[600px] 2xl:h-[700px]">
+      <img
         className=""
-        src="https://links.papareact.com/0fm"
+        src="https://wallpaperaccess.com/full/3713165.jpg"
         alt="banner img"
-        fill
       />
-      <div className="absolute top-1/2 w-full text-center">
-        <p className="text-sm sm:text-lg">Not sure where to Airbnb? Perfect.</p>
+      <div className="absolute top-1/4 w-full text-center">
+        <p className="text-sm sm:text-lg text-white">Airbnb Anywhere.</p>
 
         <button className="text-purple-500 bg-white px-10 py-4 shadow-md rounded-full font-bold my-3 hover:shadow-xl active:scale-90 transition duration-150">
           Show me
--- components/smallcard.jsx
+++ components/smallcard.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 function Smallcard({ img, location, distance }) {
   return (
-    <div className="flex items-center m-2 mt-5 space-x-4 rounded-xl cursor-pointer hover:bg-gray-100 hover:scale-105 transition transform duration-200 ease-out">
+    <div className="flex items-center m-2 mt-5 space-x-4 rounded-xl cursor-pointer hover:bg-gray-100 hover:scale-105 transition transform duration-200 ease-out hover:text-black text-white">
       {/* Left */}
       <div className="relative h-16 w-16">
         <Image className="rounded-lg" src={img} alt="small img" fill />
@@ -11,7 +11,7 @@ function Smallcard({ img, location, distance }) {
       {/* Right */}
       <div>
         <h2>{location}</h2>
-        <h3 className="text-gray-500">{distance}</h3>
+        <h3 className="text-gray-400">{distance}</h3>
       </div>
     </div>
   );
